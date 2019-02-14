b2f90c0e9b4bb275e6ae89162b570ccf7a22a11e
Kenneth Holland (RIT Student) <krh6591@ad.rit.edu>
2019-02-14 16:36:27 -0500

    Created core transform and entity system

---
+++ Engine/Core/Transform.h
@@ -0,0 +1,77 @@
+#ifndef TRANSFORM_H_
+#define TRANSFORM_H_
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/glm.hpp"
+#include "glm/gtx/quaternion.hpp"
+
+using namespace glm;
+
+class Transform
+{
+private:
+	quat rot; // Rotation quaternion
+	vec3 pos; // Position vector
+	vec3 sca; // Scale vector
+public:
+	inline Transform() :
+		pos(0.0f),
+		rot(identity<quat>()),
+		sca(1.0f)
+	{
+		// Nothing interesting to do here
+	}
+	inline Transform(const Transform& copy) :
+		pos(copy.pos),
+		rot(copy.rot),
+		sca(copy.sca)
+	{
+		// Nothing interesting to do here
+	}
+	inline Transform(vec3 position, quat rotation = identity<quat>(), vec3 scale = vec3(1.0f)) :
+		pos(position),
+		rot(rotation),
+		sca(scale)
+	{
+		// Nothing interesting to do here
+	}
+	inline ~Transform() {}
+
+	inline Transform& operator= (const Transform& copy)
+	{
+		pos = copy.pos;
+		rot = copy.rot;
+		sca = copy.sca;
+
+		return *this;
+	}
+
+	inline vec3 GetPosition() const { return pos; }
+	inline quat GetRotation() const { return rot; }
+	inline vec3 GetScale()    const { return sca; }
+
+	inline void SetPosition(vec3 position) { pos = position; }
+	inline void SetRotation(quat rotation) { rot = rotation; }
+	inline void SetScale(vec3 scale)       { sca = scale; }
+
+	inline vec3 ForwardVector() const { return rotate(rot, vec3(0.0f, 0.0f, 1.0f)); }
+	inline vec3 RightVector()   const { return rotate(rot, vec3(1.0f, 0.0f, 0.0f)); }
+	inline vec3 UpVector()      const { return rotate(rot, vec3(0.0f, 1.0f, 0.0f)); }
+
+	inline mat4 GetMatrix() const { return scale(translate(identity<mat4>(), pos) * toMat4(rot), sca); }
+
+	// Translate with respect to self
+	inline void TranslateLocal(vec3 v) { pos += rotate(rot, v); }
+	// Translate with respect to world
+	inline void TranslateGlobal(vec3 v) { pos += v; }
+	// Rotate with respect to self
+	inline void RotateLocal(quat q) { rot = normalize(cross(rot, q)); }
+	// Rotate with respect to world
+	inline void RotateGlobal(quat q) { rot = normalize(cross(q, rot)); }
+	// Add vector to scale
+	inline void ScaleAdd(vec3 v) { sca += v; }
+	// Multiply scale by vector factor
+	inline void ScaleMul(vec3 v) { sca *= v; }
+};
+
+#endif
---
+++ Engine/Core/Types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <cstdint>
+
+typedef int8_t  i08;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t  u08;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float  f32;
+typedef double f64;
+
+#endif
