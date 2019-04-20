8d0fd79f289c4b469e99ed70cf4c071a7db87909
Herman McElveen (RIT Student) <hxm1417@ad.rit.edu>
2019-04-20 17:57:56 -0400

    Last Timer Fix I Swear

--- Engine/Engine/Core/Application.cpp
+++ Engine/Engine/Core/Application.cpp
@@ -93,7 +93,7 @@ void Application::Run()
 		camera->update();
 		EventManager::ExecuteNext();
 
-		manager.updateEntities(Time.GetDeltaTime());
+		manager.updateEntities(Timer::GetDeltaTime());
 
 		glfwPollEvents();
 	}
@@ -159,7 +159,7 @@ void Application::CamMovement()
 		camera->velocity = glm::normalize(camera->velocity) * speed;
 	}
 
-	camera->position += camera->velocity * Time.GetDeltaTime();
+	camera->position += camera->velocity * Timer::GetDeltaTime();
 	camera->velocity = { 0,0,0 };
 }
 /// SUPER TEMP
